89ed5d12f6a427ed188c4f8a77d43ce37c7c0e28
Different <hello@different-name.dev>
2025-07-02 20:39:20 +1000

    feat(home-manager): add userstyle support for firefox-based browsers

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -4,9 +4,12 @@ let
   inherit (lib)
     importJSON
     mapAttrs
+    mapAttrs'
+    recursiveUpdate
     toSentenceCase
     mkIf
     mkOption
+    mkEnableOption
     getAttrFromPath
     setAttrByPath
     types
@@ -14,6 +17,8 @@ let
 
   inherit (config.catppuccin) sources;
   themes = importJSON "${sources.firefox}/themes.json";
+
+  stylusExtensionId = "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}";
 
   mkFirefoxModule =
     {
@@ -42,12 +47,91 @@ let
                   accentSupport = true;
                 }
                 // {
-                  force = mkOption {
-                    type = types.bool;
-                    default = false;
-                    description = "Forcibly override any existing configuration for Firefox Color.";
-                    example = true;
-                  };
+                  force = mkEnableOption "forcibly overriding any existing configuration for Firefox Color.";
+
+                  userstyles =
+                    catppuccinLib.mkCatppuccinOption {
+                      name = "${name} userstyles";
+                      accentSupport = true;
+                    }
+                    // {
+                      force = mkEnableOption "forcibly overriding any existing configuration for Stylus.";
+
+                      # Settings is nested twice here, much like how home-manager does it
+                      # programs.firefox.profiles.<name>.extensions.settings.<name>.settings
+                      settings = mkOption {
+                        type = types.attrsOf (
+                          types.submodule (
+                            { name, ... }:
+                            {
+                              options = {
+                                # we use mkOption instead of mkEnable option to default true
+                                enable = mkOption {
+                                  type = types.bool;
+                                  default = true;
+                                  example = false;
+                                  description = "Whether to enable ${name} Catppuccin userstyle";
+                                };
+
+                                # Since applying settings through home-manager makes the storage.js file read only,
+                                # build-stylus-settings removes the ability to configure userstyles through the stylus extension,
+                                # instead settings are defined here & are baked into the userstyles
+                                settings = mkOption {
+                                  type = types.attrsOf (
+                                    types.oneOf [
+                                      types.str
+                                      types.int
+                                      types.bool
+                                    ]
+                                  );
+                                  default = { };
+                                  description = "Userstyle setting overrides";
+                                  example = lib.literalExample ''
+                                    {
+                                      lightFlavor = "latte";
+                                      darkFlavor = "mocha";
+                                      accentColor = "mauve";
+
+                                      logo = true;
+                                      oled = false;
+                                      sponsorBlock = true;
+                                    };
+                                  '';
+                                };
+
+                                # From what I can tell, this has no function with Catppuccin userstyles
+                                # It is probably better not to include this option, to reduce confusion
+
+                                # inclusions = mkOption {
+                                #   type = types.listOf types.str;
+                                #   default = [];
+                                #   description = "Custom included sites";
+                                #   example = "*://www.aaa.com/*";
+                                # };
+
+                                exclusions = mkOption {
+                                  type = types.listOf types.str;
+                                  default = [ ];
+                                  description = "Custom excluded sites";
+                                  example = "*://www.aaa.com/*";
+                                };
+                              };
+                            }
+                          )
+                        );
+                        default = { };
+                        description = "Settings for Catppuccin userstyles";
+                      };
+
+                      # For any Stylus settings we don't provide options for, or for defining extra userstyles manually
+                      extraConfig = mkOption {
+                        type = types.attrs;
+                        default = { };
+                        description = ''
+                          Extra JSON formatted options for Stylus
+                        '';
+                      };
+                    };
                 };
             }
           );
@@ -65,6 +149,33 @@ let
                 firstRunDone = true;
                 theme = themes.${profile.flavor}.${profile.accent};
               };
+            };
+
+            settings.${stylusExtensionId} = mkIf profile.userstyles.enable {
+              inherit (profile.userstyles) force;
+              settings =
+                let
+                  userstylesConfig = {
+                    # default settings applied to all userstyles
+                    defaultSettings = {
+                      lightFlavor = profile.userstyles.flavor;
+                      darkFlavor = profile.userstyles.flavor;
+                      accentColor = profile.userstyles.accent;
+                    };
+
+                    # settings applied per userstyle
+                    # these take priority over global settings
+                    userstyles = mapAttrs' (name: value: {
+                      name = "${name} catppuccin";
+                      value = { inherit (value) enable settings exclusions; };
+                    }) profile.userstyles.settings;
+                  };
+
+                  userstylesSettings = lib.importJSON "${
+                    sources.userstyles.override { inherit userstylesConfig; }
+                  }/share/storage.js";
+                in
+                recursiveUpdate userstylesSettings profile.userstyles.extraConfig;
             };
           };
         }) cfg.profiles;
